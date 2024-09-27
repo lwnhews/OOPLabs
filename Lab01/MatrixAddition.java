5f7b1dcaf1efdb6be0c93dfcf97cd15fbb98ed12
lwnhews <luonghieu2915@gmail.com>
2024-09-27 21:36:27 +0700

    Update MatrixAddition.java

--- Lab01/MatrixAddition.java
+++ Lab01/MatrixAddition.java
@@ -7,25 +7,21 @@ public class MatrixAddition {
         int[][] mat1 = new int[rows][cols];
         int[][] mat2 = new int[rows][cols];
         int[][] sumMat = new int[rows][cols];
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j ++) {
                 mat1[i][j] = sc.nextInt();
             }
         }
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j ++) {
                 mat2[i][j] = sc.nextInt();
             }
         }
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j ++) {
                 sumMat[i][j] = mat1[i][j] + mat2[i][j];
             }
         }
-
         System.out.println("Sum of the matrices:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j ++) {
