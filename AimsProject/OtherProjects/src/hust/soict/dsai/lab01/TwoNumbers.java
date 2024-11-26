5dc15e1528cce8854d1c024d4fce5015fac794a0
lwnhews <luonghieu2915@gmail.com>
2024-11-26 14:16:37 +0700

    Add files via upload

---
+++ AimsProject/OtherProjects/src/hust/soict/dsai/lab01/TwoNumbers.java
@@ -0,0 +1,21 @@
+package hust.soict.dsai.lab01;
+
+import java.util.Scanner;
+
+public class TwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter number b: ");
+        double b = sc.nextDouble();
+        System.out.println("Sum of two numbers is: " + (a+b));
+        System.out.println("Difference of two numbers is: " + (a-b));
+        System.out.println("Product of two numbers is: " + (a*b));
+        if (b == 0) {
+            System.out.println("Cannot divide by 0");
+        } else {
+            System.out.println("Quotient of two numbers is: " + (a/b));
+        } sc.close();
+    }
+}
